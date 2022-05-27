89a864802ef4ba40b7e1ddb4c89ce6350c81aba6
the-vishal <mail007tovishal@gmail.com>
2022-05-27 19:33:03 +0530

    creating folders, running app specific commands, and downloading ZIP

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import subprocess
 import zipfile
 
 from django.shortcuts import render
@@ -35,8 +37,15 @@ class CreateProjectView(APIView):
         #can be move to celery background task
         os.chdir('/tmp')
         os.mkdir(self.project_name)
-        os.system(b_createapp)
-        os.system(f_createapp)
+        os.chdir(self.project_name)
+        # os.system(b_createapp)
+        # os.system(f_createapp)
+        subprocess.run(b_createapp.split(' '))
+        subprocess.run(f_createapp.split(' '))
+
+    def zip_shell(self):
+        os.chdir('../') 
+        subprocess.run(['zip', f'{self.project_name}.zip', '-r', f'{self.project_name}'])
 
     def zip_project(self, response):
         compressed = zipfile.ZipFile(response, 'w', zipfile.ZIP_DEFLATED)
@@ -50,8 +59,9 @@ class CreateProjectView(APIView):
         compressed.close()
 
     def process_commands(self):
-        b_createapp = self.backend.createapp.format(self.project_name)
-        f_createapp = self.frontend.createapp.format(self.project_name)
+        b_createapp = self.backend.createapp
+        f_createapp = self.frontend.createapp
+        self.make_dir_and_execute(b_createapp, f_createapp)
 
     def post(self, request):
         data = request.data
@@ -65,23 +75,27 @@ class CreateProjectView(APIView):
         code = 400
 
         self.project_name = project_name
-        self.backend = ConfigItem.objects.filter(pk=server_framework, ftype=SERVER, active=True)
-        self.frontend = ConfigItem.objects.filter(pk=client_framework, ftype=CLIENT, active=True)
+        backend = ConfigItem.objects.filter(pk=server_framework, framework__ftype=SERVER, active=True)
+        frontend = ConfigItem.objects.filter(pk=client_framework, framework__ftype=CLIENT, active=True)
 
         response = Response([{
             "status": status,
             "message": message}
         ], code)
-
-        if self.backend and self.frontend:
-            citem = CartItem.objects.create(product=product, quantity=qty)
-            cart.items.add(citem)
-            status = "success"
-            message = "Item has been added to cart"
+        if backend and frontend:
             code = 200
-            response = HttpResponse(content_type='application/zip')
-            self.zip_project(response)
-            response['Content-Disposition'] = f'attachment; filename={self.project_name}.zip'
+            self.backend = backend[0]
+            self.frontend = frontend[0]
+            self.process_commands()
+            self.zip_shell()
+            # response = HttpResponse(content_type='application/zip')
+            file = f'/tmp/{self.project_name}'
+            response = HttpResponse(open(f'{file}.zip', 'rb').read())
+
+            # self.zip_project(response)
+            # response['Content-Disposition'] = f'attachment; filename={self.project_name}.zip'
+            shutil.rmtree(file, ignore_errors=False, onerror=None)
+            os.remove(f'{file}.zip')
         return response
 
 
